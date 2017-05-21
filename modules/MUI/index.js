dcd3e01ad8f5d098f6313ffb209acc63deceac5c
Alexander Pustovalov <apustovalov@gmail.com>
2017-05-22 00:07:36 +0300

    Fix import sorting

--- modules/MUI/index.js
+++ modules/MUI/index.js
@@ -1,8 +1,8 @@
-require('./css/styles.css');
 import ThemeProvider from './components/ThemeProvider';
 import LayoutContainer from './components/LayoutContainer';
 import LayoutItem from './components/LayoutItem';
 import Button from 'material-ui/Button';
+require('./css/styles.css');
 export {
   ThemeProvider,
   LayoutContainer,
